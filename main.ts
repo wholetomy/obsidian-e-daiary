751f618de30f5af3e6e170673a99541ba6f3e3fd
wholetomy <thomasaqw@live.com>
2024-06-08 09:09:43 -0300

    new version 1.0.1, added some funcionalities like the possibility to create missed days, preventing the folder .obsidian to be selected in the options menu and the opening of the note once created

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Notice, Plugin, PluginSettingTab, Setting, moment } from 'obsidian';
+import { Notice, Plugin, PluginSettingTab, Setting, moment, TFile } from 'obsidian';
 
 // Interface para as configurações do plugin
 interface EDaiarySettings {
@@ -14,83 +14,104 @@ export default class EDaiary extends Plugin {
     settings: EDaiarySettings;
 
     async onload() {
-        // Carrega as configurações do plugin
         await this.loadSettings();
-
-        // Adiciona a aba de configurações
         this.addSettingTab(new EDaiarySettingTab(this.app, this));
+        this.addRibbonIcon('create-new', 'Create New Entry', this.createNewEntry.bind(this))
+            .addClass('my-plugin-ribbon-class');
+    }
 
-        // Cria um ícone na barra lateral
-        const ribbonIconEl = this.addRibbonIcon('create-new', 'Create New Entry', async (evt: MouseEvent) => {
-            try {
-                // Obtém a data e hora atuais
-                const today = new Date();
-                const currentYear = today.getFullYear().toString();
-                const formattedDate = moment(today).format('DD-MM-YYYY'); // Formatar como 'DD-MM-YYYY'
-                const formattedDateTime = moment(today).format('DD-MM-YYYY HH-mm'); // Formatar como 'DD-MM-YYYY HH-mm'
-                const startOfYear = new Date(today.getFullYear(), 0, 0);
-                const diff = today.getTime() - startOfYear.getTime();
-                const oneDay = 1000 * 60 * 60 * 24;
-                const dayOfYear = Math.floor(diff / oneDay);
-                const dayFolderName = `Dia ${dayOfYear} (${formattedDateTime})`;
-                const yearFolderPath = `${this.settings.baseFolderPath}/${currentYear}`;
+    async createNewEntry(evt: MouseEvent) {
+        try {
+            const today = new Date(2024, 5, 9);
+            const currentYear = today.getFullYear().toString();
+            const formattedDateTime = moment(today).format('DD-MM-YYYY HH-mm');
+            const dayOfYear = this.getDayOfYear(today);
+            const yearFolderPath = `${this.settings.baseFolderPath}/${currentYear}`;
+
+            let foldersCreated = 0;
+            let notesCreated = 0;
+
+            await this.ensureFolderExists(yearFolderPath);
+            const lastDay = await this.getLastRecordedDay(yearFolderPath);
+
+            let lastNotePath: string | null = null;
+
+            for (let day = lastDay + 1; day <= dayOfYear; day++) {
+                const currentDateTime = moment().format('DD-MM-YYYY HH-mm');
+                const dayFolderName = `Dia ${day} (${currentDateTime})`;
                 const dayFolderPath = `${yearFolderPath}/${dayFolderName}`;
                 const noteTitle = dayFolderName;
                 const notePath = `${dayFolderPath}/${noteTitle}.md`;
 
-                // Verifica se a pasta do ano já existe
-                const yearDirExists = await this.app.vault.adapter.exists(yearFolderPath);
-
-                if (!yearDirExists) {
-                    // Cria a pasta do ano
-                    await this.app.vault.createFolder(yearFolderPath);
-                    console.log(`Directory ${yearFolderPath} created successfully!`);
-                    new Notice(`Directory ${yearFolderPath} created successfully!`);
-                } else {
-                    console.log(`Directory ${yearFolderPath} already exists.`);
-                    new Notice(`Directory ${yearFolderPath} already exists.`);
+                if (await this.ensureFolderExists(dayFolderPath)) {
+                    foldersCreated++;
+                }
+                if (await this.ensureNoteExists(notePath, noteTitle)) {
+                    notesCreated++;
                 }
 
-                // Verifica se já existe uma pasta para o dia atual
-                const dayFolders = await this.app.vault.adapter.list(yearFolderPath);
-                const dayFolderExists = dayFolders.folders.some(folder => folder.includes(`Dia ${dayOfYear} (${formattedDate}`));
-
-                if (!dayFolderExists) {
-                    // Cria a pasta do dia
-                    await this.app.vault.createFolder(dayFolderPath);
-                    console.log(`Directory ${dayFolderPath} created successfully!`);
-                    new Notice(`Directory ${dayFolderPath} created successfully!`);
-                } else {
-                    console.log(`A folder for today already exists.`);
-                    new Notice(`A folder for today already exists.`);
-                }
-
-                // Verifica se a nota já existe
-                const noteExists = await this.app.vault.adapter.exists(notePath);
-
-                if (!noteExists && !dayFolderExists) {
-                    // Cria a nova nota
-                    await this.app.vault.create(notePath, ``);
-                    console.log(`Note ${noteTitle} created successfully!`);
-                    new Notice(`Note ${noteTitle} created successfully!`);
-                } else if (dayFolderExists) {
-                    console.log(`Note for today already exists in an existing folder.`);
-                    new Notice(`Note for today already exists in an existing folder.`);
-                }
-
-            } catch (error) {
-                console.error(`Failed to create directory or note:`, error);
-                new Notice(`Failed to create directory or note: ${error.message}`);
+                lastNotePath = notePath;
             }
-        });
 
-        // Perform additional things with the ribbon
-        ribbonIconEl.addClass('my-plugin-ribbon-class');
+            new Notice(`Created ${foldersCreated} folders and ${notesCreated} notes successfully!`);
+
+            if (lastNotePath) {
+                await this.openNoteIfExists(lastNotePath);
+            }
+        } catch (error) {
+            console.error(`Failed to create directory or note:`, error);
+            new Notice(`Failed to create directory or note: ${error.message}`);
+        }
     }
 
-    onunload() {
-
+    getDayOfYear(date: Date): number {
+        const startOfYear = new Date(date.getFullYear(), 0, 0);
+        const diff = date.getTime() - startOfYear.getTime();
+        const oneDay = 1000 * 60 * 60 * 24;
+        return Math.floor(diff / oneDay);
     }
+
+    async ensureFolderExists(folderPath: string): Promise<boolean> {
+        const exists = await this.app.vault.adapter.exists(folderPath);
+        if (!exists) {
+            await this.app.vault.createFolder(folderPath);
+            return true;
+        }
+        return false;
+    }
+
+    async getLastRecordedDay(yearFolderPath: string): Promise<number> {
+        const yearFolderContent = await this.app.vault.adapter.list(yearFolderPath);
+        const dayFolders = yearFolderContent.folders.filter(folder => folder.includes('Dia'));
+        if (dayFolders.length === 0) return 0;
+
+        return Math.max(...dayFolders.map(folder => {
+            const match = folder.match(/Dia (\d+)/);
+            return match ? parseInt(match[1], 10) : 0;
+        }));
+    }
+
+    async ensureNoteExists(notePath: string, noteTitle: string): Promise<boolean> {
+        const noteExists = await this.app.vault.adapter.exists(notePath);
+        if (!noteExists) {
+            await this.app.vault.create(notePath, ``);
+            return true;
+        }
+        return false;
+    }
+
+    async openNoteIfExists(notePath: string) {
+        const file = await this.app.vault.getAbstractFileByPath(notePath);
+        if (file && file instanceof TFile) {
+            const leaf = this.app.workspace.getLeaf(true);
+            await leaf.openFile(file);
+        } else {
+            console.error(`Failed to open file: ${notePath}`);
+            new Notice(`Failed to open file: ${notePath}`);
+        }
+    }
+
+    onunload() {}
 
     async loadSettings() {
         this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -114,19 +135,16 @@ class EDaiarySettingTab extends PluginSettingTab {
         const { containerEl } = this;
 
         containerEl.empty();
-
         containerEl.createEl('h2', { text: 'Settings for e-Daiary' });
 
         const folders = await this.plugin.app.vault.adapter.list('');
-        const folderPaths = folders.folders;
+        const folderPaths = folders.folders.filter(folder => !folder.includes('.obsidian'));
 
         new Setting(containerEl)
             .setName('Base Folder Path')
             .setDesc('Select the base folder where the annual folders will be created.')
             .addDropdown(dropdown => {
-                folderPaths.forEach(folder => {
-                    dropdown.addOption(folder, folder);
-                });
+                folderPaths.forEach(folder => dropdown.addOption(folder, folder));
                 dropdown.setValue(this.plugin.settings.baseFolderPath);
                 dropdown.onChange(async (value) => {
                     this.plugin.settings.baseFolderPath = value;
